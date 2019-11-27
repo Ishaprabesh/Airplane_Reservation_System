e07c7ccffda97a7b33ad0ca3c505be23f792442c
ppunekar <54676302+ppunekar@users.noreply.github.com>
2019-11-27 00:53:55 -0500

    get set changes
    added more get and set methods

--- src/Passenger.java
+++ src/Passenger.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 public class Passenger implements Serializable {
     public int age;
     private String firstName;        //do I make these private or public?
+
     private String lastName;
 
     Passenger(String firstName, String lastName, int age) {
@@ -15,11 +16,23 @@ public class Passenger implements Serializable {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public int getAge() {
         return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
